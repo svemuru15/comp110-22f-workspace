b42d33955cd0147136c358ae3dc35e416d7f1290
Sritan Vemuru <svemuru@ad.unc.edu>
2022-08-24 13:13:14 -0400

    practice

---
+++ lessons/conditionals.py
@@ -0,0 +1,13 @@
+"""An example of conditional (if-else) statements"""
+
+SECRET: int = 3
+
+print("I'm thinking of a number between 1-5, what is it?")
+guess: int = int(input("What is your guess? "))
+
+if guess == SECRET:
+    print("You guessed correctly!!!")
+else: 
+    print("Sorry, you guessed incorrectly :(")
+
+print("Game over.")
